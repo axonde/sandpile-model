60f7012f1caba78ecfdca09be19d6579f1bf2691
axonde <alex.chpak@gmail.com>
2024-10-28 23:13:45 +0300

    working a little on deque, but need to create vector instead of deque**2

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "deque/deque.h"
 #include "parser/parser.h"
 #include "utility/utility.h"
 
@@ -12,6 +13,28 @@ int main(int argc, char** argv) {
     std::cout << "Good!" << '\n';
     PrintArgs(args);
 
-    
+    Deque<int> deq;
+    deq.push_front(12);
+    deq.push_front(2);
+    deq.push_front(52);
+    deq.push_front(1);
+    Deque<int> deq2(3);
+    deq.pop_head();
+
+    Deque<Deque<int>> deq_special;
+    deq_special.push_front(deq);
+    deq_special.push_front(deq);
+
+    deq_special.push_front(deq);
+
+    std::cout << "ПЕЧАТЬ" << '\n';
+    std::cout << deq << '\n';
+    std::cout << "next" << '\n';
+    std::cout << deq2 << '\n';
+    std::cout << "next" << '\n';
+    std::cout << deq_special << '\n';
+
+    std::cout << "ЧИСТКА" << '\n';
+
     return 0;
 }
--- utility/utility.h
+++ utility/utility.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <iostream>
 #include "../parser/parser.h"
+#include "../deque/deque.h"
 
 void PrintUsage();
+
 void PrintArgs(Args args);
